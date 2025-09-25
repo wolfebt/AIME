cdbd2ff49a3ac7a4d4949e7328d08d0715c17010
WolfeBT <wolfe.tangentllc@gmail.com>
2025-09-25 14:26:28 -0500

    Delete script directory

--- script/script.js
+++
@@ -1,9 +0,0 @@
-/**
- * ==================================================================================
- * AIME General Purpose Script
- * ==================================================================================
- * This script is for any general-purpose logic needed across the application.
- * Currently, it's primarily used for the elements hub, but can be expanded.
- * ==================================================================================
- */
-console.log("AIME main script loaded.");
